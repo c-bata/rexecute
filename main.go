a17e4e7eaa6bfeb2b6a02719ffad6112510195b6
c-bata <contact@c-bata.link>
2016-12-08 03:02:42 +0900

    Detect file changes

--- main.go
+++ main.go
@@ -4,16 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
+	files := make(map[string]time.Time)
 	for s.Scan() {
-		fi, err := os.Stat(s.Text())
+		f := s.Text()
+		fi, err := os.Stat(f)
 		if err != nil {
 			return
 		}
-		mtime := fi.ModTime()
-		fmt.Println(mtime)
+		files[f] = fi.ModTime()
+		fmt.Println(f)
+	}
+
+	for {
+		for k, v := range files {
+			fi, err := os.Stat(k)
+			if err != nil {
+				return
+			}
+			mtime := fi.ModTime()
+			if mtime.After(v) {
+				// TODO: Run cmd.
+				fmt.Println(k)
+				files[k] = mtime
+			}
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
