95213bce7949a50fb9a46b80a81e7b58b8dd3954
c-bata <contact@c-bata.link>
2016-12-08 02:11:15 +0900

    Update main.go: just print mtime

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		fmt.Println(s.Text())
+		fi, err := os.Stat(s.Text())
+		if err != nil {
+			return
+		}
+		mtime := fi.ModTime()
+		fmt.Println(mtime)
 	}
 }
